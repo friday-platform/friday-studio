c389396f2ee6bd2d8e64dd6da8925894d99a7619
Eric Skram <eric@tempest.team>
2025-06-29 13:25:15 -0600

    Clean up a bunch of junk examples

--- examples/workspaces/telephone/trigger-signal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-echo "Triggering telephone signal with OpenTelemetry..."
-OTEL_DENO=true \
-OTEL_SERVICE_NAME=atlas \
-OTEL_SERVICE_VERSION=1.0.0 \
-OTEL_RESOURCE_ATTRIBUTES=service.name=atlas,service.version=1.0.0 \
-deno task atlas signal trigger telephone-message --data '{"message": "The cat sat on the mat"}'
