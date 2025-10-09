934c9310b2600701f35424e056ae70c29c05e05b
David Woolf <woolf.david@gmail.com>
2025-10-09 16:54:45 -0700

    Remove TUI (#514)

--- src/cli/commands/index.ts
+++ src/cli/commands/index.ts
@@ -1,13 +1,11 @@
 import * as agentCmd from "./agent.ts";
 import * as daemonCmd from "./daemon.ts";
 import * as diagnosticsCmd from "./diagnostics.ts";
-import * as interactiveCmd from "./interactive.tsx";
 import * as libraryCmd from "./library/index.ts";
 import * as psCmd from "./ps.ts";
 import * as serviceCmd from "./service.ts";
 import * as sessionCmd from "./session.ts";
 import * as signalCmd from "./signal.ts";
-
 import * as updateCmd from "./update.tsx";
 import * as versionCmd from "./version.ts";
 import * as workspaceCmd from "./workspace.ts";
@@ -24,5 +22,4 @@ export const commands = [
   daemonCmd,
   serviceCmd,
   diagnosticsCmd,
-  interactiveCmd,
 ];
