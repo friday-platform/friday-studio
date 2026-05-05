0a2ee4e707caf49d7901158846a5bef64a2cc52e
David Woolf <woolf.david@gmail.com>
2026-05-05 15:20:57 -0700

    $lib is not available as an import alias in packages/ui, updated to a relative import

--- packages/ui/src/lib/button.svelte
+++ packages/ui/src/lib/button.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { IconSmall } from "$lib/icons";
+  import { IconSmall } from "./icons/small";
   import type { Snippet } from "svelte";
   import type { HTMLAnchorAttributes, HTMLButtonAttributes } from "svelte/elements";
 
